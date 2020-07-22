9be055848eaa6d1fa0f2a963e9679107db1ac2dc
Justin Miller <justinmiller171@gmail.com>
2020-07-21 19:15:10 -0700

    Added api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import ImageGrid from './components/ImageGrid/ImageGrid.jsx';
 import './App.css';
 
-
 function App() {
+  const [text, setText] = useState('');
+  const api='https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=d333136e96d0d965f47b3f939d913fa8&text='
+  const api2 = '&safe_search=1&per_page=&page=&format=json&nojsoncallback=1'
   return (
     <div className="App">
       <Header />
